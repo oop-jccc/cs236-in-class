dfe3e7fd4730e9e868d2f220977ff3374c4aa8eb
Damon German <damon.german@hyland.com>
2023-08-19 16:39:36 -0500

    in class assignment

--- cs236-in-class/main/Program.cs
+++ cs236-in-class/main/Program.cs
@@ -4,31 +4,10 @@ internal static class Program
 {
     private static void Main()
     {
-        Animal dog = new Dog();
-        Animal cat = new Cat();
-        
-        Console.WriteLine("Dog makes: " + dog.MakeSound());
-        Console.WriteLine("Cat makes: " + cat.MakeSound());
-    }
-}
-
-public abstract class Animal
-{
-    public abstract string MakeSound();
-}
-
-public class Dog : Animal
-{
-    public override string MakeSound()
-    {
-        return "Woof!";
-    }
-}
-
-public class Cat : Animal
-{
-    public override string MakeSound()
-    {
-        return "Meow!";
+        // Animal dog = new Dog();
+        // Animal cat = new Cat();
+        //
+        // Console.WriteLine("Dog makes: " + dog.MakeSound());
+        // Console.WriteLine("Cat makes: " + cat.MakeSound());
     }
 }
